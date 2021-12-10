d5efb82c0713ced4b4f0d642626be06ab5a95f7e
Cong Zhang <dancerphil1994@gmail.com>
2021-12-10 15:19:52 +0800

    eject-ts: try keep git history

--- packages/eject-ts/src/index.ts
+++ packages/eject-ts/src/index.ts
@@ -2,9 +2,18 @@
 
 import glob from 'glob';
 import fs from 'fs';
+import child_process from 'child_process';
 
 const run = () => {
     const files = glob.sync('src/**/*.{js,jsx}', {nodir: true});
+
+    files.forEach(file => {
+        const content = fs.readFileSync(file, 'utf-8');
+        fs.writeFileSync(file, `// @ts-nocheck\n${content}`, 'utf-8');
+    });
+
+    console.log('Commit Part1');
+    child_process.execSync('git add . && git commit -m "eject-ts: part1" --no-verify');
 
     files.forEach(file => {
         const content = fs.readFileSync(file, 'utf-8');
@@ -12,9 +21,11 @@ const run = () => {
         const targetExt = hasJsx ? 'tsx' : 'ts';
         const filePrefix = file.slice(0, file.lastIndexOf('.'));
         const nextFile = `${filePrefix}.${targetExt}`;
-        fs.unlinkSync(file);
-        fs.writeFileSync(nextFile, `// @ts-nocheck\n${content}`, 'utf-8');
+        fs.renameSync(file, nextFile);
     });
+
+    console.log('Commit Part2');
+    child_process.execSync('git add . && git commit -m "eject-ts: part2" --no-verify');
 };
 
 run();
